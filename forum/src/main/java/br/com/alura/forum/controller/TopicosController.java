4ef8306af6f0fd063b6429734b66be16a9ef854d
carlos.ferreira <carlos.ferreira@DESKTOP-44GHSUD>
2020-02-28 13:30:35 -0300

    Projeto Inicial API rest Alura

---
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,27 @@
+package br.com.alura.forum.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+@RestController
+public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	@RequestMapping("/topicos")
+	public List<TopicoDTO> lista() {
+		
+		List<Topico> topicoss = topicoRepository.findAll();
+		
+		return TopicoDTO.converter(topicoss);
+	}
+
+}
